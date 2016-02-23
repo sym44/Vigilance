3d78a59e7f16ccc419caca067b8ef7086c50d9ca
sym44 <raymond.seki@gmail.com>
2016-02-24 00:47:32 +0800

    ## V0.8

---
+++ Vigilance/Vigilance/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Vigilance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 208);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 228);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(84, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ">",
+            ">=",
+            "=",
+            "<=",
+            "<"});
+            this.comboBox1.Location = new System.Drawing.Point(124, 228);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(82, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(212, 228);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(91, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("SimSun", 18F);
+            this.button1.Location = new System.Drawing.Point(278, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("SimSun", 18F);
+            this.button2.Location = new System.Drawing.Point(278, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(96, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 21);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "..";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "监视面板";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Vigilance/Vigilance/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vigilance
+{
+    public partial class Form1 : Form
+    {
+        private static Object selectedItem;
+        Microsoft.Office.Interop.Excel.Application theApp = Globals.ThisAddIn.Application;
+        private static Microsoft.Office.Interop.Excel.Range selectedRange;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// add condition to list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string rangeStr = textBox1.Text;
+            double standard = 0;
+            try
+            {
+                standard = Convert.ToDouble(textBox2.Text);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("输入错误:" + ex.Message);
+            }
+            
+            string relationStr = comboBox1.SelectedItem.ToString();
+            string conditionStr = rangeStr + " " + relationStr + " " + standard.ToString();
+
+            if(rangeStr == null || textBox2 == null)
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                throw new Exception("信息不完整,请重新填写.");
+            }
+
+            //同时添加表和list
+            ThisAddIn.Register(selectedRange, Relation.Equalto, standard);         
+            listBox1.Items.Add(conditionStr);
+
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        /// <summary>
+        /// remove condition
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Object selected = listBox1.SelectedItem;
+            int indexSelect = listBox1.SelectedIndex;
+            //MessageBox.Show(indexSelect.ToString());
+            //MessageBox.Show(ThisAddIn.condList.Count.ToString());
+            Condition cond = ThisAddIn.condList[indexSelect];
+
+            if(selected == null)
+            {
+                throw new Exception("没有选中任何一个项.");
+            }
+            else
+            {
+                //同时删除表和list
+                ThisAddIn.Unregister(cond);
+                listBox1.Items.Remove(selected);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedItem = listBox1.SelectedItem;
+        }
+
+        /// <summary>
+        /// select range
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            selectedRange = theApp.Selection 
+                as Microsoft.Office.Interop.Excel.Range;
+            textBox1.Text = selectedRange.AddressLocal;
+        }
+
+        private void NotifyChanges(Microsoft.Office.Interop.Excel.Range range)
+        {
+            string A;
+            string B;
+            Microsoft.Office.Tools.Excel.NamedRange nRange;
+
+            if (range.AddressLocal.Contains(':'))
+            {
+                A = range.AddressLocal.Split(':')[0];
+                B = range.AddressLocal.Split(':')[1];
+                nRange = theApp.ActiveSheet.AddNamedRange(theApp.ActiveSheet.get_Range(A, B), "");
+            }
+            else
+            {
+                A = range.AddressLocal;
+                nRange = theApp.ActiveSheet.AddNamedRange(theApp.ActiveSheet.get_Range(A, null), range.AddressLocal);
+            }
+            
+            nRange.Change += new Microsoft.Office.Interop.Excel.DocEvents_ChangeEventHandler(changesRange_Change);
+        }
+
+        void changesRange_Change(Microsoft.Office.Interop.Excel.Range Target)
+        {
+            
+            MessageBox.Show("Cell " + " changed.");
+        }
+    }
+}
